8003a1b5396d4f523eba1de1194ff73f6d16bbd9
Pushpendra Pal <palpushpendrak@johndeere.com>
2022-06-19 00:00:21 +0530

    code refactor

--- src/main/java/com/example/java8/Java8Application.java
+++ src/main/java/com/example/java8/Java8Application.java
@@ -13,32 +13,28 @@ public class Java8Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Java8Application.class, args);
-		
-		
+
 		System.out.println("Fruites stream : " + getStream().collect(Collectors.toList()));
-		 
-        // filter 1
-        Predicate<String> nofruitWordFilter = name -> !name.contains("fruit");
- 
-        // filter 2
-        Predicate<String> noLetterOFilter = name -> !name.contains("o");
- 
-        // to remove the fruites with word "fruit" and with letter "o".
-        List<String> result = getStream().filter(nofruitWordFilter)
-                .filter(noLetterOFilter)
-                .collect(Collectors.toList());
- 
-        // printing the final result
-        System.out.println("Final result : " + result);
- 
-    }
- 
-    // creating the stream of strings.
-    private static Stream<String> getStream() {
-        Stream<String> fruitesStream = Stream.of("mango", "grapes", "apple", "papaya", "jack fruit", "dragon fruit");
-        return fruitesStream;
- 
-    }
+
+		// filter 1
+		Predicate<String> nofruitWordFilter = name -> !name.contains("fruit");
+
+		// filter 2
+		Predicate<String> noLetterOFilter = name -> !name.contains("o");
+
+		// to remove the fruites with word "fruit" and with letter "o".
+		List<String> result = getStream().filter(nofruitWordFilter).filter(noLetterOFilter)
+				.collect(Collectors.toList());
+
+		// printing the final result
+		System.out.println("Final result : " + result);
+
 	}
 
+	// creating the stream of strings.
+	private static Stream<String> getStream() {
+		Stream<String> fruitesStream = Stream.of("mango", "grapes", "apple", "papaya", "jack fruit", "dragon fruit");
+		return fruitesStream;
 
+	}
+}
